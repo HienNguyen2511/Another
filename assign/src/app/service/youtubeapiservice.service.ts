880124c0e17e47231ce291b1319d6b72adc25794
HienNguyen2511 <64907379+HienNguyen2511@users.noreply.github.com>
2021-09-14 10:15:45 +0700

    assign

--- assign/src/app/service/youtubeapiservice.service.ts
+++ assign/src/app/service/youtubeapiservice.service.ts
@@ -14,7 +14,7 @@ export class YoutubeapiserviceService {
 
     const api_key_youtube = "AIzaSyA2a_H32PJdgH-T4JubBXIOUhb3-6ohaIQ";
 
-    const url_search = "https://www.googleapis.com/youtube/v3/search?part=snippet&q=" + channelName + "&type=channel&key=" + api_key_youtube + "&maxResult=50";
+    const url_search = "https://www.googleapis.com/youtube/v3/search?part=snippet&q=" + channelName + "&type=channel&key=" + api_key_youtube + "&maxResults=50";
 
     return this.httpClient.get<any>(url_search);
   }
@@ -23,7 +23,7 @@ export class YoutubeapiserviceService {
 
     const api_key_youtube = "AIzaSyCvXSyqN_dII1zeolDIlc8hi1Bnd3ZgRpY";
 
-    const url_search_video = "https://www.googleapis.com/youtube/v3/search?part=snippet&q=" + videoName + "&type=videos&key=" + api_key_youtube + "&maxResult=50";
+    const url_search_video = "https://www.googleapis.com/youtube/v3/search?part=snippet&q=" + videoName + "&type=videos&key=" + api_key_youtube + "&maxResults=50";
 
     return this.httpClient.get<any>(url_search_video);
   }
